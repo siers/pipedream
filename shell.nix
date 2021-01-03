4e9623390beeb3a1be2ef6ddaf75f5787944ff18
siers <wimuan@gmail.com>
2021-01-03 19:33:59 +0200

    add haskell-language-server to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,17 +1,22 @@
 with (import <nixpkgs> {});
 
 let
-  ghc = haskell.packages.ghc865.ghcWithPackages (packages: with packages; [
-    extra
+  ghc = haskell.packages.ghc884.ghcWithPackages (packages: with packages; [
+    base
     matrix
-    parallel
-    tuple
+    containers
     unordered-containers
+    hashable
+    extra
+    tuple
+    parallel
   ]);
 in
   mkShell {
     buildInputs = [
       ghc
+      haskell.packages.ghc884.haskell-language-server
       ws
+      cabal-install
     ];
   }
